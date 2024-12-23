6d04481f9938ab3e6cb315fcea4afa54aee4d895
Chetan3hsu <kherachetan455@gmail.com>
2024-12-23 22:13:23 +0530

    Progress so far

---
+++ haven/src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+const Navbar = () => {
+     return (
+            <nav style={{padding:'20px',backgroundColor:'green',color:'#fff'}}>
+              <h1>Haven Servicing</h1>  
+               <ul style={{listStyle:'none',display:'flex',justifyContent:'space-around', gap:'20px'}}>
+              
+               <li>Home</li> 
+               <li>About</li> 
+               <li>Contact</li>                
+                
+                
+                </ul> 
+                
+                
+                
+                </nav>
+
+
+     ) ;
+
+
+}
+
+export default Navbar;
